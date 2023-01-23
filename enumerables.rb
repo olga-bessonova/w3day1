c1c77443379f8dacceefe550cfaf34f898901065
Olga Borovikova <olga.sleepless@gmail.com>
2023-01-23 16:44:22 -0500

    add my_select and my_reject methods

--- enumerables.rb
+++ enumerables.rb
@@ -7,7 +7,23 @@ class Array
         end
         self
     end
+
+    def my_select(&prc)
+        arr = []
+        self.my_each {|el| arr << el if prc.call(el)}
+        return arr
+    end
+
+    def my_reject(&prc)
+        arr = []
+        self.my_each {|el| arr << el if !prc.call(el)}
+        return arr
+    end
+
+    
 end
+
+# Example to check my_each method
 # # calls my_each twice on the array, printing all the numbers twice.
 # return_value = [1, 2, 3].my_each do |num|
 #     puts num
@@ -22,3 +38,15 @@ end
 #     #    3
    
 #    p return_value  # => [1, 2, 3]
+
+
+# Example to check my_select method
+# a = [1, 2, 3]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
+
+
+# Example to check my_reject method
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
