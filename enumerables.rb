fefde90774da9c4725eb8438db208e8bbbc8853d
Olga Borovikova <olga.sleepless@gmail.com>
2023-01-23 17:47:40 -0500

    added my_zip

--- enumerables.rb
+++ enumerables.rb
@@ -45,10 +45,38 @@ class Array
         return array
     end
 
+    def my_zip(*arrays)
+        length = self.length
+        array_new = Array.new(length) {[]}
+
+        self.each_with_index do |el, i|
+            array_new[i] << self[i]
+            arrays.each {|array| array_new[i] << array[i]}
+        end
+        return array_new
+
+    end
+
+
     
 end
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+# Example to check my_zip method
+
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+
+
+
+# Example to check my_flatten method
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 
 # Example to check my_each method
 # # calls my_each twice on the array, printing all the numbers twice.
